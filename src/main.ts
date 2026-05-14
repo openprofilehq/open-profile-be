6d82d661f462d34776e0d4ff38001c1f8587f081
clinztouch <iordyecalvin@gmail.com>
2026-05-15 00:26:04 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
 
   app.use(helmet());
   app.use(compression());
+  app.use(cookieParser());
   const allowedOrigins = new Set(env.CORS_ORIGINS);
 
   app.enableCors({
