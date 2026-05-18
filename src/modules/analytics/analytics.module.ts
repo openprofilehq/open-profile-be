b05ece97272144d8bad1eabfbad435639a11b9e2
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-05-18 10:49:58 +0100

    feat(analytics): register new entity imports (ProfileEvent, LinkClick, SearchImpression, MetricSnapshot)

--- src/modules/analytics/analytics.module.ts
+++ src/modules/analytics/analytics.module.ts
@@ -6,14 +6,25 @@ import { AnalyticsService } from './analytics.service';
 import { Profile } from '../profile/entities/profile.entity';
 import { RedisModule } from '../../common/redis/redis.module';
 import { AuthModule } from '../auth/auth.module';
+import { ProfileEvent } from './entities/profile-event.entity';
+import { LinkClick } from './entities/link-click.entity';
+import { SearchImpression } from './entities/search-impression.entity';
+import { MetricSnapshot } from './entities/metric-snapshot.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ProfileView, Profile]),
+    TypeOrmModule.forFeature([
+      ProfileView,
+      Profile,
+      ProfileEvent,
+      LinkClick,
+      SearchImpression,
+      MetricSnapshot,
+    ]),
     RedisModule,
     AuthModule,
   ],
   controllers: [AnalyticsController],
   providers: [AnalyticsService],
 })
-export class AnalyticsModule {}
+export class AnalyticsModule { }
