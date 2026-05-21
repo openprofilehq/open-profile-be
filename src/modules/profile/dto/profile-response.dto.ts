49bd6e516508e0f19eb35a568be26f2e4d18f0f0
{Calvin Iordye <iordyecaln@gmail.com>
2026-05-21 13:40:39 +0100

    fix: update profile-response.dto.ts

--- src/modules/profile/dto/profile-response.dto.ts
+++ src/modules/profile/dto/profile-response.dto.ts
@@ -1,5 +1,5 @@
-import { ProfileContentDto } from './profile-content.dto';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ProfileContentDto } from './profile-content.dto';
 
 export class ProfileResponseDto {
   @ApiProperty({ example: '8b59d8f1-45bb-4bc9-84e0-6d5dbdc17c4a' })
