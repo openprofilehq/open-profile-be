dd3cef844df05607ffd92547071f5f1d5bef279a
maiqel1 <michaelolowe321@gmail.com>
2026-05-22 12:42:39 +0100

    refactor: add api property

--- src/modules/profile/dto/profile-content.dto.ts
+++ src/modules/profile/dto/profile-content.dto.ts
@@ -61,26 +61,32 @@ export class CtaDto {
   @IsUrl()
   url: string | null;
 
+  @ApiProperty({ example: "Let's build something", required: false })
   @IsOptional()
   @IsString()
   title?: string;
 
+  @ApiProperty({ example: 'Open to new opportunities', required: false })
   @IsOptional()
   @IsString()
   subtitle?: string;
 
+  @ApiProperty({ example: '1', required: false })
   @IsOptional()
   @IsString()
   layout?: string;
 
+  @ApiProperty({ example: 'chat', required: false, nullable: true })
   @IsOptional()
   @IsString()
   iconId?: string | null;
 
+  @ApiProperty({ example: '/profilebuilder_home/icons/chat.svg', required: false, nullable: true })
   @IsOptional()
   @IsString()
   iconSrc?: string | null;
 
+  @ApiProperty({ example: 'Chat', required: false, nullable: true })
   @IsOptional()
   @IsString()
   iconLabel?: string | null;
