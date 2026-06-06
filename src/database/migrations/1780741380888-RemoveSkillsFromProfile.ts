abfc1f1430632d1f1a08a160ec8a66fd0d629e0e
{Calvin Iordye <iordyecaln@gmail.com>
2026-06-06 11:52:49 +0100

    fix(migration): use IF EXISTS when dropping skills column

--- src/database/migrations/1780741380888-RemoveSkillsFromProfile.ts
+++ src/database/migrations/1780741380888-RemoveSkillsFromProfile.ts
@@ -4,9 +4,10 @@ export class RemoveSkillsFromProfile1780741380888 implements MigrationInterface 
   name = 'RemoveSkillsFromProfile1780741380888';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "profiles" DROP COLUMN "skills"`);
+    await queryRunner.query(
+      `ALTER TABLE "profiles" DROP COLUMN IF EXISTS "skills"`,
+    );
   }
-
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "profiles" ADD "skills" text array NOT NULL DEFAULT '{}'`,
